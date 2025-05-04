df1a53e02cd0b668c68916990efc972ff92cc0f9
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-05-04 22:02:20 +0500

    Created slider

---
+++ src/app/slider/info.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div>
+        <h1 className='text-4xl text-red-500 bg-gray-300 h-screen'>Fahad A.</h1>
+    </div>
+  )
+}
+
+export default Info
---
+++ src/app/slider/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Slider from './slider'
+import Info from './info'
+
+const Page = () => {
+  return (
+        <div>
+            <Slider />
+        </div>
+  )
+}
+
+export default Page
